366e661fd81f1b993be838aa257ab766d3739aa2
Raymond <rscylen@gmail.com>
2021-10-24 02:59:13 -0700

    finished madlibs

--- studio_projects/madlibs/script.js
+++ studio_projects/madlibs/script.js
@@ -5,6 +5,10 @@
 
     let form = document.querySelector('form');
 
+    let overlay = document.getElementById('overlay');
+    let normal = document.getElementById('normal');
+    let madlib = document.getElementById('madlib');
+
     function processData(formData) {
         let words = [];
         let emptyFields = 0;
@@ -19,15 +23,20 @@
 
         }
 
-        let overlay = document.getElementById('overlay');
-        let normal = document.getElementById('normal');
-        let madlib = document.getElementById('madlib');
+        
 
         //giving text to the div above
         if (emptyFields > 0) {
             madlib.textContent = "Incomplete form, please submit all fields";
         } else {
-            madlib.textContent = `Here are the words: ${words[0]}, ${words[1]}, ${words[2]}, ${words[3]}`;
+            madlib.textContent = 
+            `
+            Hey ${words[0]}!
+
+            So I heard you like ${words[1]}? Well I think the bongos are better! You also like the ${words[2]}? 
+            Well the keyboard is also better than that! I also heard your favorite song was ${words[3]} by ${words[4]}. I bet I could make it sound better
+            on the bongos. 
+            `;
         }
 
         normal.classList.remove('showing');
@@ -56,6 +65,17 @@
             normal.classList.add('showing');
 
         }
+    });
+
+    let exitBtn = document.getElementById("exit");
+    exitBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        overlay.classList.remove('showing');
+        normal.classList.remove('hidden');
+
+        overlay.classList.add('hidden');
+        normal.classList.add('showing');
     });
 
 })();
