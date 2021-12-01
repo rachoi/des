7a834323831235ecd3b830088ad911ade79ea122
rachoi <raymondchoi72@gmail.com>
2021-11-30 17:38:47 -0800

    temp

--- final/proto/scripts.js
+++ final/proto/scripts.js
@@ -148,7 +148,7 @@
                 }
                 
                 l2.style.color = "yellow";
-                if(realpos >= header + border1+border2) {
+                if(header + realpos >= border1+border2) {
                     l3.style.color = "yellow";
                     c2.style.backgroundColor = "yellow";
                     c2.style.border = "2px yellow solid";
